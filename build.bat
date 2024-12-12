ce0b73ed331f7fd179ca72911458a3912233b52a
Phan Viet Hoang <pvhoang@users.noreply.github.com>
2024-12-12 15:24:06 +0700

    Commit change
    Commit

--- build.bat
+++
@@ -1,8 +0,0 @@
-
-echo off
-echo ---
-@REM https://www.freecodecamp.org/news/build-a-pwa-from-scratch-with-html-css-and-javascript/
-firebase target:apply hosting nucuoitretho nucuoitretho
-
-node build detail, list, news, js, [image]
-firebase deploy --only hosting:nucuoitretho
